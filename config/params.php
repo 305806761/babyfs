fce2f765653c73bb120259f119bbbeb8dd62b1ed
Michael Tsao <caoxiang@yeah.net>
2016-10-27 11:31:45 +0800

    add new order action base on youzan

--- config/params.php
+++ config/params.php
@@ -2,4 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'youzan_appid' => '125ec687338926758c',
+    'youzan_secret' => '00bf023ac1dc1f20d1e63f9275eb862f',
 ];
--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -10,7 +10,18 @@ class OrderController extends \yii\web\Controller
 
     public function actionNew()
     {
-        Yii::$app->request->bodyParams;
+        $param = Yii::$app->request->bodyParams;
+        if (
+            isset($param['test']) &&
+            $param['test'] == false &&
+            $param['sign'] == md5(Yii::$app->params['youzan_appid'] . $param['msg'] . Yii::$app->params['youzan_secret']) &&
+            $param['status'] == 'WAIT_SELLER_SEND_GOODS' &&
+            $param['mode'] == 0 &&
+            $param['type'] == 'TRADE'
+        ) {
+            $youzan_id = $param['id'];
+            $msg = json_decode(urldecode($param['msg']));
+        }
         return '{"code":0,"msg":"success"}';
     }
 }
