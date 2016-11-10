f4e5c7a0afb59bc33c3c72fc4573c1f8d24c2887
Michael Tsao <caoxiang@yeah.net>
2016-11-10 17:23:19 +0800

    some bugs fix

--- modules/admin/controllers/UserController.php
+++ modules/admin/controllers/UserController.php
@@ -181,16 +181,17 @@ class UserController extends Controller
                 //$path_parts = pathinfo($param['user_course']['name']);
                 $file = 'uploadFile.xlsx'; //可以定义一个上传后的文件名称uploadFile.xlsx
                 $filename = '/uploads/user_course/' . $file;
+                $filePath = Yii::getAlias('@webroot' . $filename);
                 copy(
                     $_FILES['user_course']['tmp_name'],
-                    Yii::getAlias('@webroot' . $filename)
+                    $filePath
                 );
-                $data = Excel::import($filename, [
+                $data = Excel::import($filePath, [
                     'setFirstRecordAsKeys' => true, // if you want to set the keys of record column with first record, if it not set, the header with use the alphabet column on excel.
                     'setIndexSheetByName' => true, // set this if your excel data with multiple worksheet, the index of array will be set with the sheet name. If this not set, the index will use numeric.
                     'getOnlySheet' => 'sheet1', // you can set this property if you want to get the specified sheet from the excel data with multiple worksheet.
                 ]);
-                print_r($data);
+                var_dump($data);
                 die;
             }
         }
