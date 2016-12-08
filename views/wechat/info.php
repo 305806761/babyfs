c5d022e104c4823cf5c7475a6d02702a36cc3a34
dada <peng.boyu@icloud.com>
2016-12-08 17:45:12 +0800

    微信显示

--- views/wechat/info.php
+++ views/wechat/info.php
@@ -6,8 +6,12 @@
  * Time: 下午3:08
  */
 $this->title = '用户信息';
-echo 'aaaa';
-echo $model->nickname;
+
 ?>
-<h1><?=$model->nickname;?></h1>
+<div style="text-align: center">
+
+    <img src="<?=$model->headimgurl;?>" width="150" height="100" />
+
+</div>
+<h1 style="text-align: center"><?=$model->nickname;?></h1>
 
