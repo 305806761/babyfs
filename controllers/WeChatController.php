409cdd9228bfea9f28476b6035ff1fffa58b6ce0
dada <peng.boyu@icloud.com>
2016-12-08 17:48:13 +0800

    微信ok

--- controllers/WeChatController.php
+++ controllers/WeChatController.php
@@ -112,7 +112,7 @@ class WeChatController extends Controller
                                 $wechatModel->nickname = $userInfoArray['nickname'];
                                 $wechatModel->sex = $userInfoArray['sex'];
                                 $wechatModel->headimgurl = $userInfoArray['headimgurl'];
-                                $wechatModel->info = "{$userInfoArray}";
+                                $wechatModel->info = json_decode($userInfoArray);
                                 if ($wechatModel->save()) {
                                     return $this->render('/wechat/info', ['model' => $wechatModel]);
                                 } else {
