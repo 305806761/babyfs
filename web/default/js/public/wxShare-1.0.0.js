d73b6885ae0f44c15923945cb94271fe0b2f6d4e
dada <peng.boyu@icloud.com>
2016-12-29 19:12:51 +0800

    share

--- web/default/js/public/wxShare-1.0.0.js
+++ web/default/js/public/wxShare-1.0.0.js
@@ -4,9 +4,10 @@
 * wxShare.title.desc.url.imgUrl.successCallback.cancelCallback.ok()
 * @return {[type]} [description]
 */
-$.get('/we-chat/share', { url: location.href.split('#')[0] }).done(function(res) {
+//location.href.split('#')[0]
+$.get('http://course.babyfs.cn/we-chat/share/', { url: location.href.split('#')[0] }).done(function(res) {
     wx.config({
-        debug: false,
+        debug: true,
         appId: res.appId,
         timestamp: res.timestamp,
         nonceStr: res.nonceStr,
@@ -50,7 +51,14 @@ $.get('/we-chat/share', { url: location.href.split('#')[0] }).done(function(res)
     });
 
 }).fail(function() { console.log('微信分享获取后台参数ajax失败！'); });
-
+wx.ready(function(){
+    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+    alert('aaa');
+});
+wx.error(function(res){
+    // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+    document.write(res);
+});
 var wxShare = {
     ok: function() {
         var self = this;
