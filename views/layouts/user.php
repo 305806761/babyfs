6364593827d4c0290c26f5c0d984969ef4ab3b6e
malili <malili@dfgift.com>
2017-01-03 11:06:39 +0800

    新模板layouts

---
+++ views/layouts/user.php
@@ -0,0 +1,36 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\WapAsset;
+
+WapAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?= Html::encode($this->title) ?></title>
+    <meta name="viewport"content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta content="telephone=no" name="format-detection" />
+    <?php $this->head() ?>
+</head>
+
+<?php $this->beginBody() ?>
+
+<?= $content ?>
+
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
